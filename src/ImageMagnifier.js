265303cd7e790f5085c102a5506aa4d6e36ad3e1
Matt Kesler <mkesler@captechconsulting.com>
2015-11-11 16:13:43 -0500

    Add a click handler and change React to version 12

--- src/ImageMagnifier.js
+++ src/ImageMagnifier.js
@@ -1,6 +1,11 @@
+'use strict';
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var React = require('react');
 
 var Magnifier = React.createClass({
+    displayName: 'Magnifier',
 
     propTypes: {
 
@@ -37,43 +42,52 @@ var Magnifier = React.createClass({
             src: React.PropTypes.string.isRequired,
             width: React.PropTypes.number.isRequired,
             height: React.PropTypes.number.isRequired
-        }).isRequired
+        }).isRequired,
+
+        onClick: React.PropTypes.func
     },
 
-    render () {
+    handleClick: function() {
+        if(this.props.onClick) {
+            this.props.onClick();
+        }
+    },
+
+    render: function render() {
+        var _this = this;
         var props = this.props;
         var halfSize = props.size / 2;
         var magX = props.zoomImage.width / props.smallImage.width;
         var magY = props.zoomImage.height / props.smallImage.height;
-        var bgX = -(props.offsetX*magX - halfSize);
-        var bgY = -(props.offsetY*magY - halfSize);
-        var isVisible = props.offsetY < props.smallImage.height &&
-                        props.offsetX < props.smallImage.width &&
-                        props.offsetY > 0 &&
-                        props.offsetX > 0;
-        return (
-            <div style={{
-                position: 'absolute',
-                display: isVisible ? 'block' : 'none',
-                top: props.y,
-                left: props.x,
-                width: props.size,
-                height: props.size,
-                marginLeft: -halfSize + props.cursorOffset.x,
-                marginTop: -halfSize + props.cursorOffset.y,
-                backgroundColor: 'white',
-                borderRadius: props.size,
-                boxShadow: `1px 1px 6px rgba(0,0,0,0.3)`
-            }}>
-                <div style={{
+        var bgX = -(props.offsetX * magX - halfSize);
+        var bgY = -(props.offsetY * magY - halfSize);
+        var isVisible = props.offsetY < props.smallImage.height && props.offsetX < props.smallImage.width && props.offsetY > 0 && props.offsetX > 0;
+        return React.createElement(
+            'div',
+            { style: {
+                    position: 'absolute',
+                    display: isVisible ? 'block' : 'none',
+                    top: props.y,
+                    left: props.x,
                     width: props.size,
                     height: props.size,
-                    backgroundImage: `url(${props.zoomImage.src})`,
+                    marginLeft: -halfSize + props.cursorOffset.x,
+                    marginTop: -halfSize + props.cursorOffset.y,
+                    backgroundColor: 'white',
+                    borderRadius: props.size,
+                    boxShadow: '1px 1px 6px rgba(0,0,0,0.3)'
+                } },
+            React.createElement('div', {
+                style: {
+                    width: props.size,
+                    height: props.size,
+                    backgroundImage: 'url(' + props.zoomImage.src + ')',
                     backgroundRepeat: 'no-repeat',
-                    backgroundPosition: `${bgX}px ${bgY}px`,
+                    backgroundPosition: bgX + 'px ' + bgY + 'px',
                     borderRadius: props.size
-                }} />
-            </div>
+                },
+                onClick: _this.handleClick
+            })
         );
     }
 });
@@ -81,15 +95,16 @@ var Magnifier = React.createClass({
 function getOffset(el) {
     var x = 0;
     var y = 0;
-    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+    while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
         x += el.offsetLeft - el.scrollLeft;
         y += el.offsetTop - el.scrollTop;
         el = el.offsetParent;
     }
-    return { x, y };
+    return { x: x, y: y };
 }
 
 var ImageMagnifier = React.createClass({
+    displayName: 'ImageMagnifier',
 
     propTypes: {
 
@@ -114,19 +129,21 @@ var ImageMagnifier = React.createClass({
             src: React.PropTypes.string.isRequired,
             width: React.PropTypes.number.isRequired,
             height: React.PropTypes.number.isRequired
-        }).isRequired
+        }).isRequired,
+
+        onClick: React.PropTypes.func
     },
 
     portalElement: null,
 
-    getDefaultProps () {
+    getDefaultProps: function getDefaultProps() {
         return {
             size: 200,
             cursorOffset: { x: 0, y: 0 }
         };
     },
 
-    getInitialState () {
+    getInitialState: function getInitialState() {
         return {
             x: 0,
             y: 0,
@@ -135,7 +152,7 @@ var ImageMagnifier = React.createClass({
         };
     },
 
-    componentDidMount() {
+    componentDidMount: function componentDidMount() {
         document.addEventListener('mousemove', this.onMouseMove);
         if (!this.portalElement) {
             this.portalElement = document.createElement('div');
@@ -144,13 +161,13 @@ var ImageMagnifier = React.createClass({
         this.componentDidUpdate();
     },
 
-    componentWillUnmount() {
+    componentWillUnmount: function componentWillUnmount() {
         document.removeEventListener('mousemove', this.onMouseMove);
         document.body.removeChild(this.portalElement);
         this.portalElement = null;
     },
 
-    onMouseMove (e) {
+    onMouseMove: function onMouseMove(e) {
         var offset = getOffset(this.getDOMNode());
 
         this.setState({
@@ -161,20 +178,25 @@ var ImageMagnifier = React.createClass({
         });
     },
 
-    componentDidUpdate() {
-        React.render(<Magnifier
-            size={this.props.size}
-            smallImage={this.props.image}
-            zoomImage={this.props.zoomImage}
-            cursorOffset={this.props.cursorOffset}
-            {...this.state}
-        />, this.portalElement);
+    handleClick: function(e) {
+        if(this.props.onClick) {
+            this.props.onClick();
+        }
     },
 
-    render () {
-        return (
-            <img {...this.props} src={this.props.image.src} />
-        );
+    componentDidUpdate: function componentDidUpdate() {
+        React.render(React.createElement(Magnifier, _extends({
+            size: this.props.size,
+            smallImage: this.props.image,
+            zoomImage: this.props.zoomImage,
+            cursorOffset: this.props.cursorOffset,
+            onClick: this.handleClick
+        }, this.state)), this.portalElement);
+    },
+
+    render: function render() {
+        var _this = this;
+        return React.createElement('img', _extends({}, this.props, { src: this.props.image.src }));
     }
 });
 
